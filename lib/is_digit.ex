2f7e84aa293329042e8228c7630a55cacf7f0666
Mateo Sierra <m.sierra1037@hotmail.com>
2024-04-18 18:10:55 -0500

    tests/is_digit?

--- lib/is_digit.ex
+++ lib/is_digit.ex
@@ -1,5 +1,11 @@
+# https://www.codewars.com/kata/567bf4f7ee34510f69000032/elixir
+
 defmodule IsDigit do
   def digit?(s) do
-    Regex.match? ~r/^\d+$/, s
+    if String.contains?(s,"\n") do
+      false
+    else
+      Regex.match? ~r/^\d$/, s
+    end
   end
 end
--- test/is_digit_test.exs
+++ test/is_digit_test.exs
@@ -10,13 +10,16 @@ defmodule IsDigitTest do
 
   test "only numeric characters" do
     assert IsDigit.digit?("1") == true
-    assert IsDigit.digit?("123") == true
+    assert IsDigit.digit?("123") == false
   end
 
   test "numeric + non numeric characters" do
     assert IsDigit.digit?(" ") == false
     assert IsDigit.digit?("s") == false
     assert IsDigit.digit?("1a2") == false
+  end
+  test "1\n" do
+    assert IsDigit.digit?("1\n") == false
   end
 
 end
